0646fbaa5d707f33a89716bd6b3126f2ef37d86e
ronitbhathija <113222743+ronitbhathija@users.noreply.github.com>
2023-02-13 22:31:18 +0530

    Create temp.cpp

---
+++ main/temp.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run\n";
+  cout<< "SRN:PES2UG20CS284 NAME:RONIT \n" ;
+  return 0;
+}
